eef6572bc304505b8d99512865bdc670930aa896
Frank <frank.next@gmx.de>
2019-03-27 23:41:21 +0100

    .

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = function (options) {
     mode: process.env.NODE_ENV || "development",
     entry: [
       'webpack-hot-middleware/client?reload=true',
-      './src/client/index.ts'
+     // './src/client/index.ts',
+      './src/client/index3d.ts'
     ],
     module: {
       rules: [
@@ -20,7 +21,7 @@ module.exports = function (options) {
       new webpack.HotModuleReplacementPlugin(),
 
       new HtmlWebpackPlugin({
-        template: path.resolve("src", "client", "index.html")
+        template: path.resolve("src", "client", "index3d.html")
       }),
 
       // extract styles from bundle into a separate file
